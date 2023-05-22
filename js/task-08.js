5beb1f86b4e510d1ef376ad7feeaa91b6c80d73b
YuraIvashkiv <yuraivashkiv1995@gmail.com>
2023-05-22 13:09:16 +0300

    8 завд дз 6

--- js/task-08.js
+++ js/task-08.js
@@ -1,20 +1,15 @@
-const formEl = document.querySelector('.login-form');
-formEl.addEventListener('submit', formSubmit);
-
-function formSubmit(event) {
+const form = document.querySelector(".login-form");
+const userEmail = document.querySelector('[type="email"]');
+const password = document.querySelector('[type="password"]');
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const form = event.target;
-
-  if (!isFormValid(form)) {
-    alert('Усі поля мають бути заповнені.');
-    return;
+  const elements = {
+    email: userEmail.value,
+    pass: password.value
   }
-
-  const formData = Object.fromEntries(new FormData(form).entries());
-  console.log(formData);
+  if (userEmail.value === '' || password.value === '') {
+    return alert('Усі поля повинні бути заповнені!')
+  }
+  console.log(elements);
   form.reset();
-}
-
-function isFormValid(form) {
-  return [...form.elements].every((element) => element.value !== '');
-}
+});
